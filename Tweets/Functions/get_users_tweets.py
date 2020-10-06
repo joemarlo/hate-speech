0b1c117eedd2d34cf581b68d81474025dc8ab528
joemarlo <joe.marlo.09@gmail.com>
2020-10-06 12:07:00 -0400

    pull_tweets can now randomly sample ids and pull the sampled users timelines

--- Tweets/Functions/get_users_tweets.py
+++ Tweets/Functions/get_users_tweets.py
@@ -4,11 +4,14 @@ import os
 
 os.chdir("/home/joemarlo/Dropbox/Data/Projects/hate-speech")
 
+# import twitter credentials
+from twitter_credentials import consumer_key, consumer_secret, access_key, access_secret
+
 def get_users_tweets(user_id):
     # modified from https://gist.github.com/yanofsky/5436496
     # Twitter only allows access to a users most recent 3240 tweets with this method
 
-    #authorize twitter, initialize tweepy
+    # authorize twitter, initialize
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_key, access_secret)
     api = tweepy.API(auth)
@@ -17,12 +20,9 @@ def get_users_tweets(user_id):
     alltweets = []
 
     #make initial request for most recent tweets (200 is the maximum allowed count)
-    try:
-        new_tweets = api.user_timeline(user_id = user_id, count=200)
-    except:
-        return # return nothing if user id doesn't exist
+    new_tweets = api.user_timeline(user_id = user_id, count=200)
 
-    #save most recent tweets
+    #save most recent
     alltweets.extend(new_tweets)
 
     #save the id of the oldest tweet less one
@@ -33,7 +33,7 @@ def get_users_tweets(user_id):
         print(f"getting tweets before {oldest}")
 
         #all subsiquent requests use the max_id param to prevent duplicates
-        new_tweets = api.user_timeline(screen_name = screen_name,count=200,max_id=oldest)
+        new_tweets = api.user_timeline(user_id = user_id, count=200, max_id=oldest)
 
         #save most recent tweets
         alltweets.extend(new_tweets)
@@ -50,8 +50,10 @@ def get_users_tweets(user_id):
 
     return(out_df)
 
-# test the function
-get_users_tweets(25073877) #realDonaldTrump
-get_users_tweets(9024901243) #random id that doesn't work
-get_users_tweets(36523) #random id that does work
-get_users_tweets('realDonaldTrump') #this returns george's timeline
+# stuff only to run when not called via 'import' here
+if __name__ == "__main__":
+    # test the function
+    get_users_tweets(25073877) #realDonaldTrump
+    get_users_tweets(9024901243) #random id that doesn't work
+    get_users_tweets(36523) #random id that does work
+    get_users_tweets('realDonaldTrump') #this returns george's timeline
--- Tweets/pull_tweets.py
+++ Tweets/pull_tweets.py
@@ -1,11 +1,39 @@
 import tweepy
 import os
 import pandas as pd
+import numpy as np
 
 # import custom functions
 os.chdir("/home/joemarlo/Dropbox/Data/Projects/hate-speech/Tweets/Functions")
-from is_US import is_US
-from is_US import locations
+from is_US import is_US, locations
+os.chdir("/home/joemarlo/Dropbox/Data/Projects/hate-speech/Tweets/Functions")
+from get_users_tweets import get_users_tweets
 
 # reset working directory
 os.chdir("/home/joemarlo/Dropbox/Data/Projects/hate-speech")
+
+# NOTE: run twitter_credentials.py
+
+# test the function
+tmp = get_users_tweets(user_id=25073877)
+tmp.head()
+tmp = get_users_tweets(user_id=380871320)
+tmp.head()
+tmp = get_users_tweets(user_id=12)
+tmp.head()
+
+# run the function on randomly sampled tweets
+results = []
+for i in range(0, 50):
+
+    # sample for user ids
+    user_id = np.random.randint(low=1, high=1000000000, size=1)[0]
+
+    # get the tweet history
+    try:
+        result = get_users_tweets(user_id=user_id)
+    except:
+        continue
+
+    # store the results
+    results.append(result)
