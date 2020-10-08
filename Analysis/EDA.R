77054f61fe9d73ca6259237eb1a684e3fb6fc8aa
joemarlo <joe.marlo.09@gmail.com>
2020-10-08 16:00:47 -0400

    updated with latest tweet count

--- Analysis/EDA.R
+++ Analysis/EDA.R
@@ -43,7 +43,7 @@ tweets %>%
   tally() %>% 
   ggplot(aes(x = Period, y = n, color = n)) +
   geom_rect(xmin = as.Date("2016-07-01"), xmax = as.Date("2017-12-01"),
-            ymin = 0, ymax = 25000, alpha = 0.3, fill = 'grey85', size = 0) +
+            ymin = 0, ymax = 30000, alpha = 0.3, fill = 'grey85', size = 0) +
   geom_line() +
   geom_point() +
   scale_x_date(date_breaks = "1 year", date_labels = "%Y") +
@@ -59,3 +59,39 @@ tweets %>%
 ggsave("Plots/tweets_over_time.png",
        width = 8,
        height = 5)
+
+# distinct users by tweet date
+tweets %>% 
+  mutate(Period = as.Date(paste0(lubridate::year(Date), "-",
+                                 lubridate::month(Date), "-01"))) %>%
+  group_by(Period) %>%
+  summarize(n_users = n_distinct(handle),
+            .groups = 'drop') %>% 
+  ggplot(aes(x = Period, y = n_users, color = n_users)) +
+  geom_line() +
+  geom_point() +
+  scale_x_date(date_breaks = "1 year", date_labels = "%Y") +
+  scale_y_continuous(labels = scales::comma_format()) +
+  labs(title = 'Distinct users collected by tweet date',
+       subtitle = paste0('n tweets = ', scales::comma_format()(nrow(tweets)), "\n",
+                         'n users = ', scales::comma_format()(n_distinct(tweets$handle))),
+       caption = paste0("As of ", Sys.Date()),
+       x = NULL,
+       y = 'Count of users per month') +
+  theme(legend.position = 'none',
+        axis.text.x = element_text(angle = 40, hjust = 1))
+ggsave("Plots/users_over_time.png",
+       width = 8,
+       height = 5)
+
+# users by oldest tweet
+tweets %>% 
+  mutate(Period = as.Date(paste0(lubridate::year(Date), "-",
+                                 lubridate::month(Date), "-01"))) %>%
+  group_by(handle) %>% 
+  filter(Period == min(Period)) %>%
+  group_by(Period) %>% 
+  tally() %>% 
+  ggplot(aes(x = Period, y = n, color = n)) +
+  geom_line() +
+  geom_point()
