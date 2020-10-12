0461cb47ef07ed6ec905159b32590d612986bc3a
bwaheed22 <bw761@nyu.edu>
2020-10-11 21:08:48 -0400

    add dictionary and flag script
    added current hate speech dictionary and script to flag tweets from main data

---
+++ Tweets/flagtweet_script.py
@@ -0,0 +1,27 @@
+import os, json
+import glob
+from pandas import json_normalize
+
+# Set path to JSON files in directory:
+path_to_json = 'Data/'
+json_files = [pos_json for pos_json in os.listdir(path_to_json) if pos_json.endswith('.json')]
+
+# Read all JSON files in directory and store in dataframe:
+dfs = []
+for index, js in enumerate(json_files):
+    with open(os.path.join(path_to_json, js)) as json_file:
+        json_text = json.load(json_file)
+        data = pd.DataFrame(json_text)
+        dfs.append(data)
+
+df = pd.concat(dfs)
+
+# Load hatespeech:
+hatespeech = pd.read_csv("Data/hatespeech.csv", header=None)
+hatespeech = list(hatespeech[0])
+
+# Run flagtweet function on tweet data
+df = flagtweet(hatespeech, df)
+
+# Check number of flagged tweets:
+df.flagged.value_counts()
