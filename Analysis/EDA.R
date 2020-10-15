06d3edc1e29ac930051ad9933a2d50732f0008c1
joemarlo <joe.marlo.09@gmail.com>
2020-10-15 10:06:14 -0400

    updated with latest tweet numbers

--- Analysis/EDA.R
+++ Analysis/EDA.R
@@ -2,7 +2,8 @@ setwd("/home/joemarlo/Dropbox/Data/Projects/hate-speech")
 source('Plots/ggplot_settings.R')
 
 # read in and combine the data into one flat dataframe
-tweets <- map_dfr(list.files("Tweets/Data", pattern = ".json"),
+# tweets_old <- jsonlite::fromJSON("Tweets/Data/v2/US_tweets_one_one.json")
+tweets <- map_dfr(list.files("Tweets/Data/v2/raw_jsons_US_tweets_one_two", pattern = ".json"),
     function(file){
       
       # read in the data
@@ -20,8 +21,15 @@ tweets <- map_dfr(list.files("Tweets/Data", pattern = ".json"),
   # created_at field is milliseconds since 1970
   mutate(Date = as.Date(lubridate::as_datetime(created_at/1000)))
 
+# combine dataframes
+# tweets <- bind_rows(tweets_old, tweets_new)
+
 # remove duplicates
-tweets <- distinct(tweets %>% dplyr::select(-source_file))
+tweets <- distinct(tweets, id, created_at, text, location, 
+                   handle, user_id, Date, .keep_all = TRUE)
+
+# write out latest json combination to one json
+# jsonlite::write_json(jsonlite::toJSON(tweets), path = "Tweets/Data/v2/US_tweets_one_one.json")
 
 # tweets per user
 tweets %>% 
@@ -41,16 +49,25 @@ ggsave("Plots/tweets_by_user.png",
        height = 5)
 
 # tweets over time
-tweets %>% 
+tweet_tally <- tweets %>% 
   mutate(Period = as.Date(paste0(lubridate::year(Date), "-", 
                                  lubridate::month(Date), "-01"))) %>% 
   group_by(Period) %>% 
-  tally() %>% 
-  ggplot(aes(x = Period, y = n, color = n)) +
+  tally()
+tweet_tally %>% 
+  ggplot(aes(x = Period, y = n)) +
   geom_rect(xmin = as.Date("2016-07-01"), xmax = as.Date("2017-12-01"),
-            ymin = 0, ymax = 100000, alpha = 0.3, fill = 'gray90', size = 0) +
-  geom_line() +
-  geom_point() +
+            ymin = 0.9  * tweet_tally %>% filter(Period %in% seq(as.Date("2016-07-01"), as.Date("2017-12-01"), by = 'month')) %>% pull(n) %>% min(),
+            ymax = 1.1 * tweet_tally %>% filter(Period %in% seq(as.Date("2016-07-01"), as.Date("2017-12-01"), by = 'month')) %>% pull(n) %>% max(), 
+            alpha = 0.3, fill = 'gray90', size = 0) +
+  annotate(geom = 'text', 
+           x = as.Date("2016-07-01"),
+           y = 1.3 * tweet_tally %>% filter(Period %in% seq(as.Date("2016-07-01"), as.Date("2017-12-01"), by = 'month')) %>% pull(n) %>% max(),
+           label = "Observation\nperiod",
+           family = 'Helvetica', hjust = 0, size = 3, color = 'grey30', fontface = "bold"
+           ) +
+  geom_line(color = 'grey30') +
+  geom_point(color = 'grey30') +
   scale_x_date(date_breaks = "1 year", date_labels = "%Y") +
   scale_y_continuous(labels = scales::comma_format()) +
   labs(title = 'Tweets collected by tweet date',
@@ -64,6 +81,7 @@ tweets %>%
 ggsave("Plots/tweets_over_time.png",
        width = 8,
        height = 5)
+rm(tweet_tally)
 
 # distinct users by tweet date
 tweets %>% 
@@ -72,9 +90,9 @@ tweets %>%
   group_by(Period) %>%
   summarize(n_users = n_distinct(handle),
             .groups = 'drop') %>% 
-  ggplot(aes(x = Period, y = n_users, color = n_users)) +
-  geom_line() +
-  geom_point() +
+  ggplot(aes(x = Period, y = n_users)) +
+  geom_line(color = 'grey30') +
+  geom_point(color = 'grey30') +
   scale_x_date(date_breaks = "1 year", date_labels = "%Y") +
   scale_y_continuous(labels = scales::comma_format()) +
   labs(title = 'Distinct users collected by tweet date',
