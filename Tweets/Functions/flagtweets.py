4f4305545c9921809b79664805dfd34883d60bfa
bwaheed22 <bw761@nyu.edu>
2020-10-15 22:55:45 -0400

    Update flagtweets.py

--- Tweets/Functions/flagtweets.py
+++ Tweets/Functions/flagtweets.py
@@ -1,4 +1,4 @@
-# # Function to flag tweets
+# Function to flag tweets
 
 import pandas as pd
 import numpy as np
@@ -8,11 +8,18 @@ import numpy as np
 def flagtweet(hatespeech, data):
 
     # make all text in dataframe lowercase
-    data['text'] = df['text'].str.lower()
+    data['text'] = data['text'].str.lower()
 
-    # check if elements in hatespeech are in the text (1 for true, 0 for false):
-    data['flagged'] = np.multiply([any(i in words for i in hatespeech) \
-    for words in data['text'].values],1)
+    # check if elements in hatespeech are in the text (1 for true, 0 for false)
+    # accounts for different variants of how the words appear in tweets (spaces before or after the word, or both):
+    df = pd.DataFrame({})
+    df['flagged1'] = np.multiply([any(" " + i in words for i in hatespeech) for words in data['text'].values], True)
+    df['flagged2'] = np.multiply([any(i + " " in words for i in hatespeech) for words in data['text'].values],True)
+    df['flagged3'] = np.multiply([any(" " + i + " " in words for i in hatespeech) for words in data['text'].values],True)
+    df['flagged4'] = np.multiply([any( i == words for i in hatespeech) for words in data['text'].values],True)
+    df['flagged'] = df.flagged1 | df.flagged2 | df.flagged3 | df.flagged4
+
+    data = pd.concat([data.reset_index(drop = True), df['flagged']], axis = 1)
 
     return data
 
@@ -22,9 +29,9 @@ def flagtweet(hatespeech, data):
 hatespeech = ['xyz' , 'two', 'abc', 'four']
 
 # Create test dataframe with hatespeech:
-df = pd.DataFrame({'userid': ['01040', '14020','10250','10424'], 'text': ["This is xyz", "def","AbC", "FoUr"]})
+df_test = pd.DataFrame({'userid': ['01040', '14020','10250','10424', '20502'], 'text': ["This is xyz and y", "def","AbC", "FoUr", "abcdxyzasds"]})
 
 # Flag tweets:
-test_df = flagtweet(hatespeech, df)
+test_df = flagtweet(hatespeech, df_test)
 
 test_df.head()
